4c5b30d8c1802ec16b0760e4e3ea501229b1989c
Im-Kiwi <lionelmessi.rana@gmail.com>
2022-06-15 17:07:54 +0530

    added a waves design in Home page, played with some backgroundColor in home page (its not finalized yet)

--- src/Components/Home/styles.js
+++ src/Components/Home/styles.js
@@ -4,32 +4,33 @@ import { Box } from '@mui/material'
 
 export const styles = makeStyles({
     firstRow : {
+        position : 'relative',
+        backgroundColor : '#49a078',
+    },
+    navBackground : {
+        position : 'absolute',
+        width : '100vw',
+        height : '100%',
+        bottom : 0,
+        backgroundColor : '#49a078',
+    },
+    secondRow : {
+        position : 'relative',
+        backgroundColor : '#FEFEE3',
         '&::before' : {
             content : '""',
             position : 'absolute',
             width : '100vw',
-            height: '100%',
-            backgroundColor : '#f1faee'
+            height : '100%',
+            backgroundColor : '#FEFEE3',
         }
     },
-    secondRow : {
-        position : 'relative',
-    },
-    navBackground : {
-        content : '""',
-        position : 'absolute',
-        width : '100vw',
-        height : '100%',
-        backgroundColor : '#e63946'
-    },
-    
 })
 
 export const CustomLink = styled(Link)({
     textDecoration : 'none',
-    color : '#fefee3',
+    color : 'black',
     fontSize : '1.5rem',
     '&:hover' : {
-        color : '#fefee3'
     }
 })
