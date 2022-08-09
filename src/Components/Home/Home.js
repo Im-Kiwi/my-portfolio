3bb766020ffb678e33963154129d34f079697715
Im-Kiwi <lionelmessi.rana@gmail.com>
2022-08-09 16:34:37 +0530

    updated resume link

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -77,7 +77,7 @@ const Home = () => {
                 </Box>
                 <Box sx = {{mt:2}}>
                     <Button 
-                        href = 'https://drive.google.com/file/d/16f-LX0vDOZ6Q3fZytmw79DFXUa2RiIoY/view?usp=sharing' 
+                        href = 'https://drive.google.com/file/d/19iK_HX3shJr0Zd5KB4_kK67dRre7fBZ7/view?usp=sharing' 
                         rel = 'noreferrer'
                         target = '_blank'
                         className = {classes.resumeBtn}
